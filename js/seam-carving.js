f0a71beb9d79ca224a93b0080a97cc7b5493d6f8
Samuel Ernst <samuel.ernst.09@gmail.com>
2018-06-18 18:59:27 +0200

    Energy as line detecting via convolution

--- js/seam-carving.js
+++ js/seam-carving.js
@@ -10,6 +10,87 @@ var SeamCarving = (function(){
     var computeEnergy = function(x, y) {
 
         var energy = 0;
+
+        var energy_vertical = 0;
+        var energy_horizontal = 0;
+
+        // Filtre vertical
+        var filter_vertical = [
+            [-1, 0, 1],
+            [-1, 0, 1],
+            [-1, 0, 1],
+        ];
+
+        // Filtre horizontal
+        var filter_horizontal = [
+            [-1, -1, -1],
+            [ 0,  0,  0],
+            [ 1,  1,  1],
+        ];
+
+        //*/
+        var sub_3x3 = (function(width,height) {
+
+            var sub = Array.apply(null, Array(height)).map(function () { return new Array(width); });
+            var min = Infinity;
+            var max = -Infinity;
+
+            for(dy = -parseInt(height/2); dy <= parseInt(height/2); ++dy)
+            {
+                for(dx = -parseInt(width/2); dx <= parseInt(width/2); ++dx)
+                {
+                    if(!(x+dx < 0 || x+dx >= imgData.width || y+dy < 0 || y+dy >= imgData.height))
+                    {
+                        var val = (imgData.data[(imgData.width*(y+dy) + x+dx)*4 + 0] +
+                                    imgData.data[(imgData.width*(y+dy) + x+dx)*4 + 1] +
+                                    imgData.data[(imgData.width*(y+dy) + x+dx)*4 + 2])/3
+                        min = val < min ? val : min;
+                        max = val > max ? val : max;
+                        sub[1+dy][1+dx] = val;
+                    }
+                }
+            }
+
+            for(dy = 0; dy < height; ++dy)
+                for(dx = 0; dx < width; ++dx)
+                    sub[dy][dx] = 2*(sub[dy][dx]-0)/(255-0)-1;
+                    //sub[dy][dx] = 2*(sub[dy][dx]-min)/(max-min)-1;
+
+
+            return sub;
+
+        })(3,3);
+        //*/
+
+        for(dy = 0; dy < 3; ++dy)
+        {
+            for(dx = 0; dx < 3; ++dx)
+            {
+                if(!(x-1+dx < 0 || x-1+dx >= imgData.width || y-1+dy < 0 || y-1+dy >= imgData.height))
+                {
+                    energy_vertical += filter_vertical[dy][dx]*sub_3x3[dy][dx];
+                    energy_horizontal += filter_horizontal[dy][dx]*sub_3x3[dy][dx];
+                }
+            }
+        }
+
+        //console.log(filter_vertical, sub_3x3, Math.abs(energy_vertical/6));
+        //die();
+
+        energy += Math.abs(energy_vertical/6);
+        energy += Math.abs(energy_horizontal/6);
+
+        //energy = energy > 0.15 ? 1 : 0;
+
+
+        if(maxEnergy < energy)
+            maxEnergy = energy;
+
+        return energy;
+        energy = 0;
+
+        // --------------------------- Autre fonction :
+
         var nb = 0;
         var self = [
             imgData.data[(imgData.width*y + x)*4 + 0],
@@ -52,7 +133,7 @@ var SeamCarving = (function(){
 
         energy /= nb;
 
-        if(y > 0)
+        /*if(y > 0)
         {
             var legacy = energyData[imgData.width*(y-1)+x];
             if(x > 0)
@@ -61,7 +142,7 @@ var SeamCarving = (function(){
                 legacy = Math.min(legacy, energyData[imgData.width*(y-1)+x+1]);
 
             energy += legacy;
-        }
+        }*/
 
         if(maxEnergy < energy)
             maxEnergy = energy;
@@ -69,6 +150,25 @@ var SeamCarving = (function(){
         //console.log(energy);
 
         return energy; //255*(0.5*x/imgData.width + 0.5*y/imgData.height);
+    }
+
+    var synthetiseEnergyData = function() {
+        for(var y = 1; y < imgData.height; ++y)
+        {
+            for(var x = 0; x < imgData.width; ++x)
+            {
+                var legacy = energyData[imgData.width*(y-1)+x];
+                if(x > 0)
+                    legacy = Math.min(legacy, energyData[imgData.width*(y-1)+x-1]);
+                if(x < imgData.width-1)
+                    legacy = Math.min(legacy, energyData[imgData.width*(y-1)+x+1]);
+
+                energyData[imgData.width*y+x] += legacy;
+
+                if(maxEnergy < energyData[imgData.width*y+x])
+                    maxEnergy = energyData[imgData.width*y+x];
+            }
+        }
     }
 
     var loadImage = function(img, width, height) {
@@ -105,6 +205,22 @@ var SeamCarving = (function(){
                 var energy = computeEnergy(x, y)
                 energyData.push(energy);
 
+                //if(x > 2) die();
+            }
+            //die();
+        }
+
+        console.log(maxEnergy);
+
+        //synthetiseEnergyData();
+
+        console.log(maxEnergy);
+
+        for(var y = 0; y < imgData.height; ++y)
+        {
+            for(var x = 0; x < imgData.width; ++x)
+            {
+                var energy = energyData[imgData.width*y + x];
                 if(y == imgData.height-1 && minEnergyLastRow > energy)
                 {
                     minEnergyLastRow = energy;
@@ -116,6 +232,7 @@ var SeamCarving = (function(){
                 energyImgData.data[(imgData.width*y + x)*4 + 2] = 255*energy/maxEnergy; // B
             }
         }
+
 
         console.log(minEnergyLastRow, minEnergyLastRowIdx);
 
